34e813016c5a66cca5a1b43c076c676b956de349
thetechgirlie <ogochukwu.nnaemezie.247214@unn.edu.ng>
2024-11-15 12:09:48 +0100

    Remove extra Dart files from repository

--- ex12.dart
+++
@@ -1,9 +0,0 @@
-
-import 'dart:io';
-void main(List<String> args) {
-  
-  //stdout.write('Enter your name:');
-  print("enter your name:");
-  String? name = stdin.readLineSync();
-  print('hello $name');
-}
--- ex5.dart
+++
@@ -1,5 +0,0 @@
-void main(List<String> args) {
-  final myFavFruits = ['Banana', 'Apple', 'Orange', 'Watermelon', 'Cashew','Mango','Strawberry'];
-  myFavFruits.sort();
-  print(myFavFruits);
-}
